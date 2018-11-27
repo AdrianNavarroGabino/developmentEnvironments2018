dd666ca0a09e82be33ab4e34bc84c18387dc07cf
Adrián Navarro <adnaga94@gmail.com>
2018-11-27 16:33:06 +0100

    116 - ¡Hola mundo!

---
+++ C++ProgrammingLanguage/Acepta-el-reto/Hello.cpp
@@ -0,0 +1,18 @@
+// https://www.aceptaelreto.com/problem/statement.php?id=116
+// Adrián Navarro Gabino
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int times;
+    cin >> times;
+    
+    for(int i = 0; i < times; i++)
+    {
+        cout << "Hola mundo." << endl;
+    }
+	return 0;
+}
