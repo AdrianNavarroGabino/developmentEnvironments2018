672b602faf1537bdaade5f7fa0520e9ee0fc657d
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-03-29 20:13:02 +0100

    Exercise 3 - Averagen of 4 integer numbers

---
+++ annex-Python/03-Average.py
@@ -0,0 +1,11 @@
+# Adrián Navarro Gabino
+
+average = 0
+
+for i in range(4):
+    number = float(input("Enter a number: "))
+    average += number
+
+average /= 4
+
+print("Average: ", (round(average, 3)))
